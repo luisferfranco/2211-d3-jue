977706063706ed0f1218c5cb6d5f28b300d9eb03
Luis F Franco <luisferfranco@gmail.com>
2022-12-08 20:42:49 -0600

    04-Scatter-Inicial

--- 04-scatter/js/app.js
+++ 04-scatter/js/app.js
@@ -9,24 +9,3 @@ const svg = graf
   .attr("width", ancho)
   .attr("height", alto)
   .attr("class", "fig")
-
-var cx = ancho / 2
-var cy = alto / 2
-var r = alto / 4
-
-const circle = svg
-  .append("circle")
-  .attr("cx", cx)
-  .attr("cy", cy)
-  .attr("r", r)
-  .attr("fill", "#003566")
-
-const mover = (dx, dy) => {
-  cx += dx
-  cy += dy
-  circle.transition().duration(500).attr("cx", cx).attr("cy", cy)
-}
-
-const color = (dc) => {
-  circle.transition().duration(2000).attr("fill", dc)
-}
